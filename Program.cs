279d1de78f3c6d87ed537f871dcc6e191d35dfae
Per Rovegård <per@rovegard.se>
2019-11-18 09:09:22 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+
+namespace jpegtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var img = Image.FromFile("test.jpg"))
+	    {
+                Console.WriteLine(img.HorizontalResolution);
+                Console.WriteLine(img.VerticalResolution);
+            }
+        }
+    }
+}
